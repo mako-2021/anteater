d840f9ca1e876f884e9cdd43ba0c623f6ea9e2a4
Henry Glenn <henryglenn43@gmail.com>
2021-06-03 17:45:26 +1200

    created the base of our project

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const server = express() 
+const express = require('express') // Bringing in code thats been written for us.
+const hbs = require('express-handlebars') // Bringing in code thats been written for us.
 
-server.use(express.static('public'))
-server.use(express.urlencoded({extended: false}))
+const server = express() // Creating our server using the package that was installed.
 
-server.engine('hbs', hbs({extname: 'hbs'})) // tells us that all the hbs files are usable. 
-server.set('view engine', 'hbs')
+server.use(express.static('public')) // Allows the public folder to be active
+server.use(express.urlencoded({ extended: false })) // URL picking up body of request (exp in forms)
 
+server.engine('hbs', hbs({ extname: 'hbs' })) // tells us that all the hbs files are usable.
+server.set('view engine', 'hbs') // allowing us to use handlebars
 
-
-
-
-
-
+server.get('/', (req, res) => { // ./ in current directory  / -> from root ../ go up a directory to find file
+  res.send('Hello World')
+})
 
 module.exports = server
-
-
