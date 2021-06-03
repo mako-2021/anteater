834e02f2ce2fbd4a1de04d635e25392c535712a7
Henry Glenn <henryglenn43@gmail.com>
2021-06-04 11:01:23 +1200

    updated quiz page test

--- server.test.js
+++ server.test.js
@@ -2,17 +2,28 @@ const request = require('supertest')
 const cheerio = require('cheerio')
 const server = require('./server')
 
-// test ('testing home page', () => {
-
-// })
-
 test('test setup working #1', () => {
-    expect.assertions(2)
-    expect(true).toBeTruthy()
-    expect(false).toBeFalsy()
+  expect.assertions(2)
+  expect(true).toBeTruthy()
+  expect(false).toBeFalsy()
 })
 
-test('GET /', () => {
-    expect.assertions(1)
-    expect('/').toContain('Hello World')
+test('Home page test', () => {
+  expect.assertions(1)
+  return request(server)
+    .get('/')
+    .then(result => {
+      expect(result.text).toContain('Hello World')
+      return null
+    })
+})
+
+test('Quiz page test', () => {
+  expect.assertions(1)
+  return request(server)
+    .get('/')
+    .then(result => {
+      expect(result.text).toContain('Hello World')
+      return null
+    })
 })
