8deb5c1edab8ad391cb91ca3b052616f69a2c571
farrelje <farrel.burns@gmail.com>
2021-06-04 14:14:11 +1200

    Fix linting errors

--- lib.js
+++ lib.js
@@ -1,15 +1,14 @@
 const fs = require('fs')
 
 function getQuiz (cb, path = './quiz.json') {
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) return null
-        const quizData = JSON.parse(data) 
-        console.log(quizData['anteater'])
-        cb(null, quizData["anteater"])
-    })
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) return null
+    const quizData = JSON.parse(data)
+    console.log(quizData.anteater)
+    cb(null, quizData.anteater)
+  })
 }
 
-
 module.exports = {
-    getQuiz
+  getQuiz
 }
