b7879c5726b85f6ccff0c3ad2ee8877d37d14ff0
Laura Corkin <nzlaura.williams@gmail.com>
2021-06-04 14:44:26 +1200

    tidied up lib code

--- lib.js
+++ lib.js
@@ -8,14 +8,6 @@ function getQuiz (cb, path = './quiz.json') {
     })
 }
 
-// function populateQuiz (cb, path = '.quiz.json') {
-//     fs.readFile(path, 'utf8', (err, data) => {
-//         if (err) return null
-//         const quizData = JSON.parse(data)
-
-//     })
-// }
-
 
 module.exports = {
     getQuiz
